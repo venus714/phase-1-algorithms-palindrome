5ad23e99123a366eb80e5a7be908cd9213135d18
Student <venus.kabera@student.moringaschool.com>
2022-12-09 13:47:51 +0300

    grt back to lab

--- index.js
+++ index.js
@@ -1,18 +1,40 @@
 function isPalindrome(word) {
-  // Write your algorithm here
+  const arrayOfLetters = word.split('');
+  const reverseLetters = arrayOfLetters.reverse()
+  const reverseString  = reverseLetters.join('')
+
+  if (word == reverseString){
+
+    return true
+
+  } else {
+    return false
+  }
+
 }
+
 
 /* 
   Add your pseudocode here
+if word= palindrome then print true
+else print false
 */
 
 /*
   Add written explanation of your solution here
+  if the word is palindrome then we print out True,else we print false
 */
 
 // You can run `node index.js` to view these console logs
 if (require.main === module) {
   // add your own custom tests in here
+  console.log('Expecting: false')
+  console.log('=>', isPalindrome('boy'));
+
+  console.log('Expecting: true')
+  console.log('=>', isPalindrome('abba'));
+
+
   console.log("Expecting: true");
   console.log("=>", isPalindrome("racecar"));
 
